d20362345e1523643c470ccf4264ee2ece41f2b6
jc <emclab2011@gmail.com>
2014-01-23 16:06:33 -0600

    JC: added style.

--- db/migrate/20131028210051_create_src_plantx_plants.rb
+++ db/migrate/20131028210051_create_src_plantx_plants.rb
@@ -3,30 +3,30 @@ class CreateSrcPlantxPlants < ActiveRecord::Migration
     create_table :src_plantx_plants do |t|
       t.string :name
       t.string :short_name
-      t.integer :last_updated_by_id
-      t.boolean :active, :default => true
       t.text :address
-      t.string :phone
-      t.string :fax
-      t.string :web
       t.text :primary_contact
-      t.string :primary_email
       t.string :primary_cell
+      t.string :phone
       t.text :tech_contact
-      t.string :tech_email
       t.string :tech_cell
-      t.integer :quality_system_id
-      t.text :equip
-      t.date :src_since
-      t.text :main_product
       t.text :tech_ability
       t.text :customer_service
       t.text :production_capacity
-      t.string :employee_num
-      t.string :revenue
       t.text :sourced_product
-
+      t.text :equip
+      t.integer :quality_system_id
+      t.string :employee_num
+      t.boolean :active, :default => true
+      t.string :revenue
+      t.date :src_since
       t.timestamps
+      t.integer :last_updated_by_id
+      t.string :web
+      t.string :primary_email
+      t.string :fax  
+      t.text :main_product    
+      t.string :tech_email
+      
     end
     
     add_index :src_plantx_plants, :name
