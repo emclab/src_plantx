b26c82ed2b9442d0254ae6b42e023ae9a7fe8ceb
jc <emclab2011@gmail.com>
2014-05-08 21:48:47 -0500

    JC: added @erb_code to create/update. spec passes.

--- app/controllers/src_plantx/plants_controller.rb
+++ app/controllers/src_plantx/plants_controller.rb
@@ -13,6 +13,7 @@ module SrcPlantx
     def new
       @title = t('New Plant')
       @plant = SrcPlantx::Plant.new()
+      @erb_code = find_config_const('plant_new_view', 'src_plantx')
     end
   
     def create
@@ -21,6 +22,7 @@ module SrcPlantx
       if @plant.save
         redirect_to URI.escape(SUBURI + "/authentify/view_handler?index=0&msg=Successfully Saved!")
       else
+        @erb_code = find_config_const('plant_new_view', 'src_plantx')
         flash[:notice] = t('Data Error. Not Saved!')
         render 'new'
       end
@@ -29,6 +31,7 @@ module SrcPlantx
     def edit
       @title = t('Update Plant')
       @plant = SrcPlantx::Plant.find_by_id(params[:id])
+      @erb_code = find_config_const('plant_edit_view', 'src_plantx')
     end
   
     def update
@@ -37,6 +40,7 @@ module SrcPlantx
       if @plant.update_attributes(params[:plant], :as => :role_update)
         redirect_to URI.escape(SUBURI + "/authentify/view_handler?index=0&msg=Successfully Updated!")
       else
+        @erb_code = find_config_const('plant_edit_view', 'src_plantx')
         flash[:notice] = t('Data Error. Not Updated!')
         render 'edit'
       end
